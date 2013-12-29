9eef0e5244198277899d17e58f6660c9e08993b3
Kelly Crabbé <kelly.crabbe@gmail.com>
2013-12-29 02:27:52 +0100

    added some extra tags

--- tags.lua
+++ tags.lua
@@ -31,6 +31,8 @@ local function Status(unit)
 end
 oUF.Tags.Methods['yna:status'] = Status
 
+-----------------------------
+-- Leader func
 oUF.Tags.Methods['yna:leader'] = function(unit)
   if(UnitIsGroupLeader(unit)) then
     return '|cffffff00L|r'
@@ -38,11 +40,42 @@ oUF.Tags.Methods['yna:leader'] = function(unit)
 end
 oUF.Tags.Events['yna:leader'] = 'PARTY_LEADER_CHANGED'
 
+-----------------------------
+-- colorize power
 oUF.Tags.Methods['yna:colorpp'] = function(unit)
   local _, str = UnitPowerType(unit)
   local coloredmana = _COLORS.power[str]
   return coloredmana and string.format('|cff%02x%02x%02x', coloredmana[1] * 255, coloredmana[2] * 255, coloredmana[3] * 255)
 end
+
+-----------------------------
+-- colorize HP
+oUF.Tags.Methods['yna:colorhp'] = function(unit)
+  local colored = oUF.colors.health
+  return colored and string.format('|cff%02x%02x%02x', colored[1] * 255, colored[2] * 255, colored[3] * 255)
+end
+
+-----------------------------
+-- health func
+oUF.Tags.Methods['yna:health'] = function(unit)
+  if(not UnitIsConnected(unit) or UnitIsDead(unit) or UnitIsGhost(unit)) then return end
+
+  local min, max = UnitHealth(unit), UnitHealthMax(unit)
+  if(min ~= 0 and min ~= max) then
+    return '-' .. SI(max - min)
+  else
+    return SI(max)
+  end
+end
+oUF.Tags.Events['yna:health'] = oUF.Tags.Events.missinghp
+-- oUF.ColorGradient
+-----------------------------
+-- Shortname
+oUF.Tags.Methods['yna:shortname'] = function(unit)
+  local name = UnitName(unit)
+  return (string.len(name) > 10) and string.gsub(name, '%s?(.)%S+%s', '%1. ') or name
+end
+oUF.Tags.Events['yna:shortname'] = 'UNIT_NAME_UPDATE UNIT_REACTION UNIT_FACTION'
 
 ------------------------------------------------------------------------
 -- Tags - Class specific
@@ -54,3 +87,16 @@ oUF.Tags.Methods['yna:druidpower'] = function(unit)
   end
 end
 oUF.Tags.Events['yna:druidpower'] = oUF.Tags.Events.missingpp
+-----------------------------
+-- ComboPoints
+oUF.Tags.Methods['yna:cp'] = function(unit)
+  local cp = UnitExists("vehicle") and GetComboPoints("vehicle", "target") or GetComboPoints("player", "target")
+  cpcol = {"8AFF30","FFF130","FF6161"}
+  if cp == 1 then            return "|cff"..cpcol[1].."_|r"
+  elseif cp == 2 then        return "|cff"..cpcol[1].."_ _|r"
+  elseif cp == 3 then        return "|cff"..cpcol[1].."_ _|r |cff"..cpcol[2].."_|r"
+  elseif cp == 4 then        return "|cff"..cpcol[1].."_ _|r |cff"..cpcol[2].."_ _|r"
+  elseif cp == 5 then        return "|cff"..cpcol[1].."_ _|r |cff"..cpcol[2].."_ _|r |cff"..cpcol[3].."_|r"
+  end
+end
+oUF.Tags.Events['yna:cp'] = 'UNIT_COMBO_POINTS'
